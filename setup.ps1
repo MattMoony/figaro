3b99f043b0bb208520438e4c9782c71aee2b0a1c
Matthias Monschein <monschein.matthias@gmail.com>
2021-01-11 11:23:32 +0100

    :hammer: added windows setup script, updated linux setup

--- setup.ps1
+++ setup.ps1
@@ -1,1 +1,53 @@
-# To-Do, write PowerShell setup script ... 
+function log {
+    param (
+        $Type,
+        $Msg
+    )
+    switch ($Type) {
+        "error"     { $c = "Red"; $s = "-"; }
+        "warning"   { $c = "Yellow"; $s = "!"; }
+        "success"   { $c = "Green"; $s = "+"; }
+        default     { $c = "Cyan"; $s = "*"; }
+    }
+    Write-Host "[$(Get-Date -Format 'yyyy-MM-dd~HH:mm')|$s] " -ForegroundColor $c -NoNewline
+    Write-Host "$Msg"
+}
+
+log "success" 'Setting up "Figaro"'
+
+log "info" "Checking requirements ... "
+log "info" "Checking python/pip installation ... "
+$PyVersion = "$(python --version)"
+if (-Not $PyVersion.StartsWith("Python 3")) {
+    log "error" "No python3 installation could be found!"
+    exit 1
+}
+log "success" "Functioning python3 setup detected!"
+python -m pip --version >$null
+if (-Not $?) {
+    log "error" "No python3 installation could be found!"
+    exit 1
+}
+log "success" "Functioning pip3 setup detected!"
+log "info" "Checking node/npm installation ... "
+node --version >$null
+if (-Not $?) {
+    log "error" "No node setup could be found!"
+    exit 1
+}
+log "success" "Functional node installation found!"
+npm --version >$null
+if (-Not $?) {
+    log "error" "No npm installation found!"
+    exit 1
+}
+log "success" "Functioning npm setup found!"
+
+log "info" "Installing python requirements ... "
+python -m pip install -r .\requirements-windows.txt
+
+log "info" "Installing node requirements ... "
+Set-Location figaro/gui/web/
+npm i
+
+log "success" 'Finished setting up "Figaro"'
--- setup.sh
+++ setup.sh
@@ -25,29 +25,32 @@ log() {
 log "success" "Setting up \"Figaro\""
 
 log "info" "Checking requirements ... "
-log "info" "Checking python installation ... "
+log "info" "Checking python/pip installation ... "
 /usr/bin/env python3 --version >/dev/null
 if [[ $? -ne 0 ]]; then
     log "error" "No python3 installation could be found!"
     exit 1
-else
-    log "success" "Functioning python3 setup detected!"
 fi
+log "success" "Functioning python3 setup detected!"
+/usr/bin/env python3 -m pip --version >/dev/null
+if [[ $? -ne 0 ]]; then
+    log "error" "No pip3 installation could be found!"
+    exit 1
+fi
+log "success" "Functioning pip3 setup detected!"
 log "info" "Checking node/npm installation ... "
 /usr/bin/node --version >/dev/null
 if [[ $? -ne 0 ]]; then
     log "error" "No node setup could be found!"
     exit 1
-else 
-    log "success" "Functional node installation found!"
 fi
+log "success" "Functional node installation found!"
 /usr/bin/npm --version >/dev/null
 if [[ $? -ne 0 ]]; then
     log "error" "No npm installation found!"
     exit 1
-else
-    log "success" "Functioning npm setup found!"
 fi
+log "success" "Functioning npm setup found!"
 
 log "info" "Installing python requirements ... "
 /usr/bin/env python3 -m pip install -r ./requirements-unix.txt
@@ -57,4 +60,3 @@ cd figaro/gui/web/
 npm i
 
 log "success" "Finished setting up \"Figaro\""
-
