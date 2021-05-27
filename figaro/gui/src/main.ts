982189bd0963897cd52f96337c9cd9c0695f7e43
MattMoony <m4ttm00ny@gmail.com>
2021-05-27 20:09:27 +0200

    :children_crossing: en route to gui alpha

--- figaro/gui/src/main.ts
+++ figaro/gui/src/main.ts
@@ -1,23 +1,42 @@
 import { app, BrowserWindow } from 'electron';
 import fs from 'fs';
 import path from 'path';
+import serve from 'electron-serve';
+
+const loadURL = serve({ directory: path.resolve(__dirname, '..', 'web', 'public'), });
+let win: BrowserWindow;
 
 import conf from './config/conf.json';
 
 function createWindow (): void {
   const sconf: string = path.resolve(__dirname, '..', '..', 'server', 'conf.json');
-  const win: BrowserWindow = new BrowserWindow({
+  win = new BrowserWindow({
     width: 800,
     height: 600,
     webPreferences: {
       nodeIntegration: true,
     },
+    show: false,
   });
+
+  // process.env['ELECTRON_DISABLE_SECURITY_WARNINGS'] = 'true';
   // win.webContents.openDevTools();
   win.removeMenu();
-  win.loadURL(`http://${conf.host}:${conf.port}`);
+  // win.loadURL(`http://${conf.host}:${conf.port}`);
+  // win.loadURL(`http://localhost:5000/`);
+  loadURL(win);
   win.webContents.executeJavaScript(`localStorage.setItem('no-logout', true);`);
   win.webContents.executeJavaScript(`localStorage.setItem('tkn', '${fs.readFileSync(path.resolve(__dirname, '..', '.tkn'))}');`);
+
+  win.once('ready-to-show', () => win.show());
 }
 
-app.whenReady().then(createWindow);
+app.on('ready', createWindow);
+
+app.on('window-all-closed', () => {
+  if (process.platform !== 'darwin') app.quit();
+});
+
+app.on('activate', () => {
+  if (win === null) createWindow();
+});
