eaf7aa50ecea7ec1fbd2bcfd326999403f704cf8
Matthias Monschein <monschein.matthias@gmail.com>
2023-06-17 12:05:36 +0200

    :construction: work on new channel code

--- figaro/channel.py
+++ figaro/channel.py
@@ -3,9 +3,9 @@ Channels the altered input data to the output devices.
 """
 
 import struct
-import time
-from threading import Lock, Thread
-from typing import Any, Dict, List, Optional
+from multiprocessing import Event, Manager, Process
+from multiprocessing.managers import ListProxy
+from typing import List, Optional
 
 import numpy as np
 
@@ -16,314 +16,123 @@ from figaro.sounds.sound import Sound
 from figaro.transformer import Transformer
 
 
-class Channel(Thread):
+class Channel:
     """
     The channel between input and output.
     """
-
-    transf: Transformer
+    
+    trf: Transformer
     """The transformer applied to the input."""
-    ist: List[Device]
-    """The input devices."""
-    ost: List[Device]
-    """The output devices."""
-    buff: np.ndarray
+    buf: np.ndarray
     """The current buffer."""
-    filters: List[Filter]
-    """Filters to be applied."""
-    sounds: List[Sound]
-    """Sounds to be played."""
-    _running: bool
-    """Is the channel active?"""
-    _ist_mut: Lock
-    """Mutex for the input stream."""
-    _ost_mut: Lock
-    """Mutex for the output streams."""
-    _fil_mut: Lock
-    """Mutex for the filters list."""
-    _sou_mut: Lock
-    """Mutex for the sounds list."""
 
-    def __init__(self, transf: Optional[Transformer] = None, ist: List[Device] = [], ost: List[Device] = [], 
-                 filters: List[Filter] = [], sounds: List[Sound] = [], *args: List[Any], **kwargs: Dict[str, Any]) -> None:
+    inputs: ListProxy[Device]
+    """The input devices."""
+    outputs: ListProxy[Device]
+    """The output devices."""
+    filters: ListProxy[Filter]
+    """Filters to be applied."""
+    sounds: ListProxy[Sound]
+    """Sounds to be played."""
+
+    __dead: Event
+    """Indicates whether the channel is dead."""
+    __mgr: Manager
+    """The manager used to create the proxies."""
+    __p: Process
+    """The process running the channel."""
+
+    def __init__(self, trf: Optional[Transformer] = None, inputs: Optional[List[Device]] = None,
+                 outputs: Optional[List[Device]] = None, filters: Optional[List[Filter]] = None,
+                    sounds: Optional[List[Sound]] = None) -> None:
         """
         Initialize a new Channel object.
 
         Args:
-            transf (Optional[Transformer]): The transformer to be applied to the input.
-            ist (List[Device]): The input devices. Default is [].
-            ost (List[Device]): The output devices. Default is [].
+            trf (Optional[Transformer]): The transformer to be applied to the input.
+            inputs (List[Device]): The input devices. Default is [].
+            outputs (List[Device]): The output devices. Default is [].
             filters (List[Filter]): The filters to be applied. Default is [].
             sounds (List[Sound]): The sounds to be played. Default is [].
         """
-        super(Channel, self).__init__(*args, **kwargs)
-        self.transf: Transformer = transf or Transformer()
-        self.ist: List[Device] = ist
-        self.ost: List[Device] = ost
-        self.buff: np.ndarray = np.array([])
-        self.filters: List[Filter] = filters
-        self.sounds: List[Sound] = sounds
-        self._running: bool = False
-        self._ist_mut: Lock = Lock()
-        self._ost_mut: Lock = Lock()
-        self._fil_mut: Lock = Lock()
-        self._sou_mut: Lock = Lock()
+        self.__mgr = Manager()
+        self.__dead = self.__mgr.Event()
+
+        self.trf = trf or Transformer()
+        self.buf = np.array([])
+        self.inputs = self.__mgr.list(inputs or [])
+        self.outputs = self.__mgr.list(outputs or [])
+        self.filters = self.__mgr.list(filters or [])
+        self.sounds = self.__mgr.list(sounds or [])
 
     def start(self) -> None:
         """
-        Start the audio channeling process.
-        """
-        if not self.ist or not self.ost:
-            raise IOError('Missing I/O devices!')
-        return super().start()
+        Start the channel.
 
-    def run(self) -> None:
+        Raises:
+            IOError: If the channel is already running.
         """
-        Read audio from the input, run it through the 
+        if not self.__dead.set():
+            raise IOError('Channel is already running')
+        self.__dead.clear()
+        self.__p = Process(target=self.__run)
+        self.__p.start()
+
+    def stop(self) -> None:
+        """
+        Stop the channel.
+
+        Raises:
+            IOError: If the channel is not running.
+        """
+        if self.__dead.set():
+            raise IOError('Channel is not running')
+        self.__dead.set()
+        self.__p.join()
+
+    def __run(self) -> None:
+        """
+        Read audio from the input streams, run it through the 
         transformer and write the result to the output streams.
         """
-        self._running = True
-        while self._running:
-            self._ist_mut.acquire()
-            self.buff = np.zeros(params.BUF)
-            for i in self.ist:
-                self.buff += np.asarray(struct.unpack('f'*params.BUF, i.read(params.BUF, exception_on_overflow=False)))
-            self.buff /= len(self.ist)
-            self._ist_mut.release()
-            self.buff = self.transf.apply_all(self.buff)
-            self._fil_mut.acquire()
-            for f in self.filters:
-                self.buff = f(self.buff)
-            self._fil_mut.release()
-            self._sou_mut.acquire()
-            dels = []
-            for i, s in enumerate(self.sounds):
-                so_raw = s.read(params.BUF)
-                if so_raw == b'':
-                    dels.append(i)
-                    continue
-                so = np.asarray(struct.unpack(s.format*(len(so_raw)//s.f_size), so_raw)).astype(np.float32)
-                so /= 2**(8*s.f_size-1)
-                so = np.hstack((so, np.zeros(params.BUF-len(so))))*s.amp
-                self.buff = np.average([self.buff, so], axis=0, weights=[.8,.2])
-            for d in reversed(dels):
-                del self.sounds[d]
-            self._sou_mut.release()
-            raw = struct.pack('f'*len(self.buff), *self.buff)
-            self._ost_mut.acquire()
-            for o in self.ost:
-                o.write(raw)
-            self._ost_mut.release()
+        while not self.__dead.is_set():
+            self.__read()
+            self.__transform()
+            self.__write()
 
-    def add_ist(self, i: Device) -> None:
+    def __read(self) -> None:
         """
-        Add an input device.
-        
-        Args:
-            i (Device): The input device to be added.
+        Read audio from the input streams.
         """
-        self._ist_mut.acquire()
-        if i in self.ist:
-            self._ist_mut.release()
-            raise Exception("Input Stream is already being used!")
-        self.ist.append(i)
-        self._ist_mut.release()
+        self.buf = np.zeros(params.BUF)
+        n: int = 0
+        for inp in self.inputs:
+            self.buf += np.asarray(struct.unpack('f'*params.BUF, inp.read(params.BUF, exception_on_overflow=False)))
+            n += 1
+        self.buf /= n   # probably better due to parallelism
 
-    def get_ists(self) -> List[Device]:
+    def __transform(self) -> None:
         """
-        Get all input devices.
-        
-        Returns:
-            List[Device]: The input devices.
+        Transform the input - i.e. run it through the
+        transform, add sound effects, etc.
         """
-        self._ist_mut.acquire()
-        cp = list(self.ist)
-        self._ist_mut.release()
-        return cp
+        self.buf = self.trf.apply_all(self.buf)
+        for f in self.filters:
+            self.buf = f(self.buf)
+        for s in reversed(self.sounds):
+            raw = s.read(params.BUF)
+            if raw == b'':
+                self.sounds.remove(s)
+                continue
+            raw = so = np.asarray(struct.unpack(s.format*(len(raw)//s.f_size), raw)).astype(np.float32)
+            raw /= 2**(8*s.f_size-1)
+            raw = np.hstack((so, np.zeros(params.BUF-len(so))))*s.amp
+            self.buf = np.average([self.buf, raw,], axis=0, weights=[.8, .2,])
 
-    def del_ist(self, dev_ind: int) -> None:
+    def __write(self) -> None:
         """
-        Remove an input device.
-        
-        Args:
-            dev_ind (int): The index of the device to be removed.
+        Write the result to the output streams.
         """
-        self._ist_mut.acquire()
-        if dev_ind not in map(lambda d: d.indi, self.ist):
-            self._ist_mut.release()
-            raise Exception("Input Stream isn't being used!")
-        self.ist = list(filter(lambda i: i.indi != dev_ind, self.ist))
-        if not self.ist:
-            self.kill()
-        self._ist_mut.release()
-
-    def add_ost(self, o: Device) -> None:
-        """
-        Add an output device.
-        
-        Args:
-            o (Device): The output device to be added.
-        """
-        self._ost_mut.acquire()
-        if o in self.ost:
-            self._ost_mut.release()
-            raise Exception("Output Stream is already being used!")
-        self.ost.append(o)
-        self._ost_mut.release()
-
-    def get_osts(self) -> List[Device]:
-        """
-        Get all output devices.
-        
-        Returns:
-            List[Device]: The list of output devices.
-        """
-        self._ost_mut.acquire()
-        cp = list(self.ost)
-        self._ost_mut.release()
-        return cp
-
-    def del_ost(self, dev_ind: int) -> None:
-        """
-        Remove an output device.
-        
-        Args:
-            dev_ind (int): The index of the output device to be removed.
-        """
-        self._ost_mut.acquire()
-        if dev_ind not in map(lambda d: d.indo, self.ost):
-            self._ost_mut.release()
-            raise Exception("Output Stream isn't being used!")
-        self.ost = list(filter(lambda o: o.indo != dev_ind, self.ost))
-        if not self.ost:
-            self.kill()
-        self._ost_mut.release()
-
-    def kill(self) -> None:
-        """
-        Stop channeling audio.
-        """
-        self._sou_mut.acquire()
-        self.sounds = []
-        self._sou_mut.release()
-        self._running = False
-
-    def kill_all(self) -> None:
-        """
-        Stop all audio channels.
-        """
-        for i in self.ist:
-            i.stop_stream()
-            i.close()
-        for o in self.ost:
-            o.stop_stream()
-            o.close()
-
-    def add_filter(self, fil: Filter) -> None:
-        """
-        Add a filter to the channel.
-        
-        Args:
-            fil (Filter): The filter to be added.
-        """
-        self._fil_mut.acquire()
-        self.filters.append(fil)
-        self._fil_mut.release()
-
-    def get_filters(self) -> List[Filter]:
-        """
-        Get all currently applied filters.
-        
-        Returns:
-            List[Filter]: The list of filters.
-        """
-        self._fil_mut.acquire()
-        cp = list(self.filters)
-        self._fil_mut.release()
-        return cp
-
-    def del_filter(self, i: int) -> None:
-        """
-        Stop a filter that's currently applied.
-        
-        Args:
-            i (int): The index of the filter to be removed.
-        """
-        self._fil_mut.acquire()
-        del self.filters[i]
-        self._fil_mut.release()
-
-    def del_all_filters(self) -> None:
-        """
-        Stop all currently applied filters.
-        """
-        self._fil_mut.acquire()
-        # self.filters = []
-        for i in range(len(self.filters)-1, -1, -1):
-            del self.filters[i]
-        self._fil_mut.release()
-
-    def add_sound(self, sound: Sound) -> None:
-        """
-        Add a sound effect to the channel.
-        
-        Args:
-            sound (Sound): The sound effect to be added.
-        """
-        self._sou_mut.acquire()
-        self.sounds.append(sound)
-        self._sou_mut.release()
-
-    def get_sounds(self) -> List[Sound]:
-        """
-        Get all currently playing soundeffects.
-        
-        Returns:
-            List[Sound]: The list of sound effects.
-        """
-        self._sou_mut.acquire()
-        cp = list(self.sounds)
-        self._sou_mut.release()
-        return cp
-
-    def del_sound(self, i: int) -> None:
-        """
-        Stop a sound effect that's currently running.
-        
-        Args:
-            i (int): The index of the sound effect to be removed.
-        """
-        self._sou_mut.acquire()
-        del self.sounds[i]
-        self._sou_mut.release()
-
-    def del_all_sounds(self) -> None:
-        """
-        Stop all currently running sound effects.
-        """
-        self._sou_mut.acquire()
-        # self.sounds = []
-        for i in range(len(self.sounds)-1, -1, -1):
-            del self.sounds[i]
-        self._sou_mut.release()
-
-    def is_running(self) -> bool:
-        """
-        Returns `True` if the channel is currently active.
-        
-        Returns:
-            bool: Whether the channel is currently active or not.
-        """
-        return self._running
-
-    def __str__(self):
-        """
-        Returns a string representation of the channel.
-        
-        Returns:
-            str: The string representation.
-        """
-        return 'Channel: {{{}}} --> {{{}}} | {} ... '.format(
-                ', '.join(sorted([str(i.indi) for i in self.ist])) if self.ist else '-', 
-                ', '.join(sorted([str(o.indo) for o in self.ost])) if self.ost else '-', 
-                'running' if self._running else 'stopped')
+        raw = struct.pack('f'*len(self.buf), *self.buf)
+        for out in self.outputs:
+            out.write(raw)
